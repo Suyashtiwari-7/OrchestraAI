3e3f6bf17df6a6f1f4022f789360d15f747a0425
Suyash Tiwari <suyashtiwari7272@gmail.com>
2026-08-18 04:56:55 +0530

    chore: clean up root directory clutter

--- push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "GIT_EXE=%LOCALAPPDATA%\Programs\Git\cmd\git.exe"
-if not exist "%GIT_EXE%" set "GIT_EXE=git"
-
-"%GIT_EXE%" add .
-"%GIT_EXE%" commit -m "Update %date% %time%" 2>nul
-"%GIT_EXE%" push origin main
-echo [+] Updates pushed to GitHub!
